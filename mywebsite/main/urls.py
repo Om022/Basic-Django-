38b8717d730e4880cb0143b5844bbbdf7a414126
Om022 <85447429+Om022@users.noreply.github.com>
2024-07-12 02:23:57 +0530

    Made some changes

--- mywebsite/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('submit_form/', views.submit_form, name='submit_form'),
-    path('submissions/', views.submissions_list, name='submissions_list'),
-]
-
--- mywebsite/mywebsitee.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE mywebsite;
-CREATE USER 'mywebsiteuser'@'localhost' IDENTIFIED BY 'Stifler@99';
-GRANT ALL PRIVILEGES ON mywebsite.* TO 'mywebsiteuser'@'localhost';
-FLUSH PRIVILEGES;
